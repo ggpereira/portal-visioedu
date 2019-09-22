bfd71fe2bcd7b35ab16758c38e429cae274178e7
odcmello <odmello@inf.ufsm.br>
2019-09-22 14:47:59 -0300

    Capturando localização user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'visioEdu-Portal';
+
+  infoUser:any;
+
+  constructor(private http: HttpClient){
+    this.http.get<{query: string}>('http://ip-api.com/json')
+    .subscribe( data => {
+      this.infoUser = data;
+      console.log(this.infoUser.city)
+    });
+  }
 
   // sidenavEvents(str) {
   //   console.log(str);
