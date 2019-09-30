c9b1fb6c046da0cbccee15041c86ab50659d6127
ggpereira <ggomesper@gmail.com>
2019-09-30 00:09:24 -0300

    gráficos na home, finalmente funcionando \o/

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { CubejsClient } from '@cubejs-client/ngx';
-import { ILocation } from '../shared/models/location';
-import { IEstadoStatistics } from '../shared/models/statisticsEstado';
-import { ICidadeStatistics } from '../shared/models/statisticsCidade';
-import { LocationService } from '../services/location.service';
-import { EnemService } from '../services/enem.service';
-import { IMediasCidade } from '../shared/models/mediasEnem';
-import { Subject } from 'rxjs';
-import { StatisticsCidadeService } from '../services/statistics-cidade.service';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
 
+export interface ChartConf {
+  chartLabels: Label[];
+  legend: boolean;
+  chartPlugins: any;
+  chartData: ChartDataSets[];
+  chartOptions: ChartOptions;
+}
 
 @Component({
   selector: 'app-charts',
@@ -21,109 +19,15 @@ import { Label } from 'ng2-charts';
 export class ChartsComponent implements OnInit {
   @Input() chartType: string;
   @Input() titulo: string;
-  @Input() query: string;
+  @Input() subHeader: string;
+  @Input() chartConfig: ChartConf;
+  @Input() iconName: string;
+  @Input() colors: any;
 
-  local: ILocation;
-  statsCidade: ICidadeStatistics;
-  dataMediasCidade: IMediasCidade;
-  cardSubHeader: string;
-  chartValues: number[];
-
-  private querySubject;
-
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-  };
-
-  public barChartLabels: Label[];
-  public barChartType: ChartType;
-  public barChartLegend = true;
-  public barChartPlugins = [];
-  public barChartData: ChartDataSets[];
-
-
-  constructor(private locationService: LocationService,
-              private statisticsCidadeService: StatisticsCidadeService, private enemService: EnemService) { }
+  constructor() {}
 
 
   ngOnInit() {
-    this.querySubject = new Subject();
-    this.resultChanged();
-    this.querySubject.next(this.query);
-  }
-
-  resultChanged() {
-    if (this.query === 'mediasQuery' && this.chartType === 'bar') {
-      this.mediasQuery();
-    } else if (this.query === 'statsQuery' && this.chartType === 'bar') {
-      this.statsQuery();
-    }
-  }
-
-  mediasQuery() {
-    this.locationService.getLocation().subscribe((dataLocal: ILocation) => {
-      this.local = dataLocal;
-      console.log(this.local);
-      this.enemService.getMediaCidade(this.local.city, this.local.region).subscribe((dataMedias: IMediasCidade) => {
-        this.dataMediasCidade = dataMedias;
-        this.barChartLabels = ['Humanas', 'Naturais', 'Matemática', 'Redação', 'Linguagens', 'Geral'];
-        this.barChartPlugins = [];
-        this.barChartType = 'bar';
-        this.chartValues = [];
-        this.chartValues.push(this.dataMediasCidade.mediaCh);
-        this.chartValues.push(this.dataMediasCidade.mediaCn);
-        this.chartValues.push(this.dataMediasCidade.mediaMat);
-        this.chartValues.push(this.dataMediasCidade.mediaRedacao);
-        this.chartValues.push(this.dataMediasCidade.mediaLc);
-        this.chartValues.push(this.dataMediasCidade.mediaGeral);
-
-        this.barChartData = [{
-          data: this.chartValues,
-          label: 'Médias'
-        }];
-
-        this.cardSubHeader = this.local.city;
-      });
-
-    });
-  }
-
-  statsQuery() {
-    // this.locationService.getLocation().subscribe((dataLocal: ILocation) => {
-    //   this.local = dataLocal;
-
-    //   this.statisticsCidadeService.getStatsCidade(this.local.city, this.local.regionName).subscribe((statsCidade: ICidadeStatistics) => {
-    //     this.statsCidade = statsCidade;
-
-    //     this.chartData = [
-    //       {
-    //         name: 'Escolas com Água',
-    //         value: 100 - (this.statsCidade.porcentagemAguaInexistente * 100)
-    //       },
-    //       {
-    //         name: 'Escolas com Esgoto',
-    //         value: 100 - (this.statsCidade.porcentagemEsgotoInexistente * 100)
-    //       },
-    //       {
-    //         name: 'Escolas com Energia',
-    //         value: 100 - (this.statsCidade.porcentagemEnergiaInexistente * 100)
-    //       },
-    //       {
-    //         name: 'Escolas com Coleta Periodica',
-    //         value: this.statsCidade.porcentagemLixoColetaPeriodica * 100
-    //       },
-    //       {
-    //         name: 'Escolas com Biblioteca',
-    //         value: this.statsCidade.porcentagemBiblioteca * 100
-    //       },
-    //       {
-    //         name: 'Escolas com Internet',
-    //         value: this.statsCidade.porcentagemInternet * 100
-    //       }
-    //     ];
-    //     this.cardSubHeader = this.local.city;
-    //   });
-    // });
   }
 
   onSelect(event) {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { EnemService } from '../services/enem.service';
+import { StatisticsService } from '../services/statistics.service';
+import { StatisticsCidadeService } from '../services/statistics-cidade.service';
+import { ILocation } from '../shared/models/location';
+import { ChartConf } from '../charts/charts.component';
+import { ICidadeStatistics } from '../shared/models/statisticsCidade';
+import { IMediasCidade } from '../shared/models/mediasEnem';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +16,133 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   chartType: string;
   titulo: string;
-  constructor() { }
+
+  location: ILocation;
+  chartConf: ChartConf;
+  statsCidade: ICidadeStatistics;
+  dataMediasCidade: IMediasCidade;
+  chartValues: number[];
+  cardSubHeader: string;
+  pieChartConfAguaInexistente: ChartConf;
+  pieChartConfInternet: ChartConf;
+  pieChartConfEsgoto: ChartConf;
+  pieChartConfEnergia: ChartConf;
+
+
+  public colors = [
+    {
+      backgroundColor: [
+        '#9932CC',
+        '#9370DB',
+        '#E6E6FA',
+        '#9975B9',
+        '#BFA8D3',
+      ]
+    }
+  ];
+
+  constructor(
+    private enemService: EnemService,
+    private statisticsService: StatisticsService,
+    private statisticsCidadeService: StatisticsCidadeService,
+    private locationService: LocationService) { }
 
   ngOnInit() {
+    this.locationService.getLocation().subscribe((locationData: ILocation) => {
+      this.location = locationData;
+      this.getMediaCidade(this.location.city, this.location.region);
+      this.getEstatisticasCidade(this.location.city, this.location.region);
+    });
+  }
+
+
+  getMediaCidade(cidade: string, estado: string){
+    this.enemService.getMediaCidade(cidade, estado).subscribe((dataMedias: IMediasCidade) => {
+      this.dataMediasCidade = dataMedias;
+      this.chartType = 'bar';
+      this.chartValues = [];
+      this.chartValues.push(this.dataMediasCidade.mediaCh);
+      this.chartValues.push(this.dataMediasCidade.mediaCn);
+      this.chartValues.push(this.dataMediasCidade.mediaMat);
+      this.chartValues.push(this.dataMediasCidade.mediaRedacao);
+      this.chartValues.push(this.dataMediasCidade.mediaLc);
+      this.chartValues.push(this.dataMediasCidade.mediaGeral);
+
+      this.chartConf = {
+        chartLabels: ['Humanas', 'Naturais', 'Matemática', 'Redação', 'Linguagens', 'Geral'],
+        chartPlugins: [],
+        legend: true,
+        chartData: [{
+          data: this.chartValues,
+          label: 'Médias'
+        }],
+        chartOptions: {
+          responsive: true,
+        }
+      };
+
+      this.cardSubHeader = this.location.city;
+    });
+  }
+
+  getEstatisticasCidade(cidade: string, estado: string){
+    this.statisticsCidadeService.getStatsCidade(cidade, estado).subscribe((statsCidade: ICidadeStatistics) => {
+      this.statsCidade = statsCidade;
+      this.pieChartConfAguaInexistente = {
+        chartLabels: [['Possui'], ['Não possui']],
+        chartPlugins: [],
+        legend: true,
+        chartData: [{
+          data: [(1 - this.statsCidade.porcentagemAguaInexistente) * 100, this.statsCidade.porcentagemAguaInexistente * 100],
+        }],
+        chartOptions: {
+          responsive: true,
+        }
+      };
+
+      this.pieChartConfEnergia = {
+        chartLabels: [['Possui'], ['Não possui']],
+        chartPlugins: [],
+        legend: true,
+        chartData: [{
+          data: [(1 - this.statsCidade.porcentagemEnergiaInexistente) * 100, this.statsCidade.porcentagemEnergiaInexistente * 100],
+        }],
+        chartOptions: {
+          responsive: true,
+        }
+      };
+
+      this.pieChartConfInternet = {
+        chartLabels: [['Possui acesso'], ['Não possui acesso']],
+        chartPlugins: [], 
+        legend: true, 
+        chartData: [{
+          data: [this.statsCidade.porcentagemInternet * 100 , (1 - this.statsCidade.porcentagemInternet) * 100],
+        }],
+        chartOptions: {
+          responsive: true,
+        }
+      };
+
+      this.pieChartConfEsgoto = {
+        chartLabels: [['Possui'], ['Não possui']],
+        chartPlugins: [],
+        legend: true,
+        chartData: [{
+          data: [(1 - this.statsCidade.porcentagemEsgotoInexistente) * 100, this.statsCidade.porcentagemEsgotoInexistente]
+        }],
+        chartOptions: {
+          responsive: true
+        }
+      };
+
+      this.cardSubHeader = this.location.city;
+    });
+  }
+
+  getChartColors(): any {
+    var customColors = [{ backgroundColor: ['#9370DB', '#E6E6FA', '#9975B9', '#BFA8D3'] }];
+    return customColors;
   }
 
 }
