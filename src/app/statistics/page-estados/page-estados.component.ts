01d2fdbb142d20f6814facdbcb465500bae87313
ggpereira <ggomesper@gmail.com>
2019-10-06 02:02:25 -0300

    view estáticas por estado agrupados

--- src/app/statistics/page-estados/page-estados.component.ts
+++ src/app/statistics/page-estados/page-estados.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ILocation } from 'src/app/shared/models/location';
 import { ChartsComponent, ChartConf } from 'src/app/charts/charts.component';
 import { LocationService } from 'src/app/services/location.service';
 import { EnemService } from 'src/app/services/enem.service';
 import { IMediasEnem } from 'src/app/shared/models/enem';
+import { StatisticsService } from 'src/app/services/statistics.service';
+import { IEstatisticasEstado } from 'src/app/shared/models/estatisticas';
 
 @Component({
   selector: 'app-page-estados',
@@ -14,8 +17,10 @@ export class PageEstadosComponent implements OnInit {
   chartType: string;
   titulo: string;
   location: ILocation;
-  chartsConf: Array<ChartConf>;
+  barChartConf: ChartConf;
+  pieChartsConf: Array<ChartConf>;
   mediasEnemData: IMediasEnem;
+  estatisticas: IEstatisticasEstado;
 
   public colors = [
     {
@@ -29,20 +34,183 @@ export class PageEstadosComponent implements OnInit {
     }
   ];
 
-  constructor(private locationService: LocationService, private enemService: EnemService) { }
+  constructor(
+    private locationService: LocationService,
+    private enemService: EnemService,
+    private estatisticasService: StatisticsService
+    ) { }
 
   ngOnInit() {
     this.locationService.getLocation().subscribe((locationData: ILocation) => {
       this.location = locationData;
       this.getMediasEstado(this.location.region);
+      this.getDadosInfraestruturaEscolas(this.location.region);
     });
   }
 
-  getMediasEstado(nomeEstado: string) {
+  getDadosInfraestruturaEscolas(nomeEstado: string) {
+    this.estatisticasService.getEstatisticasEstado(nomeEstado).subscribe((data: Array<IEstatisticasEstado>) => {
+      this.estatisticas = data[0];
+      this.pieChartsConf = [
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [(1 - this.estatisticas.porcentagemAguaInexistente) * 100, this.estatisticas.porcentagemAguaInexistente * 100],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem água',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [(1 - this.estatisticas.porcentagemEnergiaInexistente) * 100, this.estatisticas.porcentagemEnergiaInexistente * 100],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem energia',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui acesso'], ['Não possui acesso']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [this.estatisticas.porcentagemInternet * 100, (1 - this.estatisticas.porcentagemInternet) * 100],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem internet',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [(1 - this.estatisticas.porcentagemEsgotoInexistente) * 100, this.estatisticas.porcentagemEsgotoInexistente]
+          }],
+          chartOptions: {
+            responsive: true
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem esgoto',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [
+              this.estatisticas.porcentagemLaboratorioInformatica * 100,
+              (1 - this.estatisticas.porcentagemLaboratorioInformatica) * 100
+            ],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem lab Informática',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [
+              this.estatisticas.porcentagemSalaAtendimentoEspecial * 100,
+              (1 - this.estatisticas.porcentagemSalaAtendimentoEspecial) * 100
+            ],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Sala para atendimento Especial',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [
+              this.estatisticas.porcentagemBiblioteca * 100,
+              (1 - this.estatisticas.porcentagemBiblioteca) * 100
+            ],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que possuem biblioteca',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Recicla'], ['Não Recicla']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [
+              this.estatisticas.porcentagemLixoRecicla * 100,
+              (1 - this.estatisticas.porcentagemLixoRecicla) * 100
+            ],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Escolas que reciclam lixo',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        },
+        {
+          chartLabels: [['Possui'], ['Não Possui']],
+          chartPlugins: [],
+          legend: true,
+          chartData: [{
+            data: [
+              this.estatisticas.porcentagemLaboratorioCiencias * 100,
+              (1 - this.estatisticas.porcentagemLaboratorioCiencias) * 100
+            ],
+          }],
+          chartOptions: {
+            responsive: true,
+          },
+          chartType: 'pie',
+          title: 'Laboratório de Ciências',
+          subHeader: nomeEstado,
+          iconName: 'pie_chart'
+        }
+      ];
+    });
+  }
+
+getMediasEstado(nomeEstado: string) {
     this.enemService.getMediasEstados(nomeEstado).subscribe((mediasData: Array<IMediasEnem>) => {
       this.mediasEnemData = mediasData[0];
-      this.chartsConf = [
-        {
+
+      this.barChartConf = {
           title: 'Médias Enem',
           iconName: 'bar_chart',
           chartType: 'bar',
@@ -64,14 +232,13 @@ export class PageEstadosComponent implements OnInit {
             responsive: true
           },
           subHeader: nomeEstado
-        }
-      ];
+        };
     });
   }
 
-  selectedStateValue(state) {
-    console.log('Estado selecionado:', state);
+selectedStateValue(state) {
     this.getMediasEstado(state);
+    this.getDadosInfraestruturaEscolas(state);
   }
 
 }
