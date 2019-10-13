7b7b0d4b62ece06afbbcc5909a03b00616192c9c
ggpereira <ggomesper@gmail.com>
2019-10-13 12:08:30 -0300

    serviço para obter cidades

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { ILocation } from '../shared/models/location';
+import { ICidade } from '../shared/models/cidade';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,28 @@ import { ILocation } from '../shared/models/location';
 export class LocationService {
 
   constructor(private http: HttpClient) {}
-
+  /*
+    Obtém a localização com base no provedor
+  */
   getLocation(): Observable<ILocation> {
     return this.http.get<ILocation>(environment.apiLocation);
   }
 
+  /*
+    Obtém cidades
+  */
+  getCidades(nome?: string, uf?: string): Observable<Array<ICidade>> {
+    let params = new HttpParams();
 
-  getCidades():  
+    if (nome !== undefined) {
+      params = params.append('nome', nome);
+    }
+
+    if (uf !== undefined) {
+      params = params.append('uf', uf);
+    }
+
+    return this.http.get<Array<ICidade>>(environment.host + 'municipios', { params });
+  }
 
 }
