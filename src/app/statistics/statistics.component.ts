c3e94a0adc60e9de16ab6cf9bf1b908d8ee299db
ggpereira <ggomesper@gmail.com>
2019-11-06 19:28:25 -0300

    remove código não utilizado no componente

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -8,20 +8,6 @@ import { StatisticsService } from '../services/statistics.service';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
-  myData: Array<any>;
-  constructor(private stastisticService: StatisticsService) { }
+  constructor() { }
   ngOnInit() {}
-
-  goViewEscolas() {
-    console.log('ir para view escolas' );
-  }
-
-  goViewEstados() {
-    console.log('ir para view estados');
-  }
-
-  goViewCidades() {
-    console.log('ir para view cidades');
-  }
-
 }
